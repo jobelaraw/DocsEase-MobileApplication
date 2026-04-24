8b99fdb5f3a83864c25e562287fd81e3414796b7
Meagan Enguerra <meagansilvano@gmail.com>
2026-04-25 02:25:29 +0800

    Stop tracking .env file

--- docsease/lib/chatbot.dart
+++ docsease/lib/chatbot.dart
@@ -28,7 +28,7 @@ class _ChatBotScreenState extends State<ChatBotScreen> {
   int? _speakingIndex;
   final List<_ChatMessage> _messages = [
     _ChatMessage(
-      text: "Hi! 👋 Ako si DocEase Bot at Nandito ako para tulungan ka sa mga dokumento, permit, at anumang prosesong kailangan mo. Ano ang gusto mong gawin ngayon?",
+      text: "Hi! Ako si DocEase Bot at Nandito ako para tulungan ka sa mga dokumento, permit, at anumang prosesong kailangan mo. Ano ang gusto mong gawin ngayon?",
       isUser: false,
       time: _formatTime(DateTime.now()),
     ),
